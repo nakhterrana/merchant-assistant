cf09a1fc68646ada677b34e31ce571f427e85cda
Masad <muhammadmasad@folio3.com>
2023-08-16 13:16:43 +0500

    comments added

--- textToSql.py
+++ textToSql.py
@@ -5,6 +5,7 @@ from google.cloud import bigquery
 from vertexai.language_models import TextGenerationModel, \
     CodeGenerationModel
 
+# List of apology phrases for potential disruptions
 apology_phrases = [
     "I sincerely apologize for any inconvenience caused by the unexpected interruption in our conversation.",
     "My deepest apologies for the sudden disruption in our chat. Please know that I'm here to assist you.",
@@ -27,24 +28,26 @@ apology_phrases = [
     "I apologize for any disturbance you experienced. Let's continue with our interaction.",
     "My sincerest apologies for the chat interruption. Your understanding is greatly appreciated."
 ]
-
+# Initialize Flask app
 app = Flask(__name__)
 
+# Constants for dataset and project IDs
 dataset_id = "folio3bigaihackathon"
 PROJECT_ID = "merchant-assistant-395407"
+
+# Initialize Vertex AI
 vertexai.init(project=PROJECT_ID, location="us-central1")
+# Initialize BigQuery client
 client = bigquery.Client(project=PROJECT_ID)
 
-
+# Function to execute SELECT queries on BigQuery
 def run_select_query(sql_statement):
     client = bigquery.Client()
     query_job = client.query(sql_statement)
     query_results = query_job.result()
     return query_results.to_dataframe()
 
-
-vertexai.init(project=PROJECT_ID, location="us-central1")
-
+# Schema details for various tables
 TABLE_SCHEMA_STR = f'''
 [SCHEMA details for table `{PROJECT_ID}.{dataset_id}.bc_customer`]:
 Full table name: {PROJECT_ID}.{dataset_id}.bc_customer - Column: customer_id - Data Type: INT64 - Primary Key: False - Foreign Key: True - Description: The unique identifier for each customer.
@@ -62,28 +65,30 @@ Full table name: `{PROJECT_ID}.{dataset_id}.bc_order` - Column: sub_total_includ
 Full table name: `{PROJECT_ID}.{dataset_id}.bc_order` - Column: total_including_tax - Data Type: NUMERIC - Primary Key: False - Foreign Key: False - Description: The total order amount including taxes.
 Full table name: `{PROJECT_ID}.{dataset_id}.bc_order` - Column: last_updated_datetime - Data Type: DATETIME - Primary Key: False - Foreign Key: False - Description: The date and time when the order was last updated.
 [SCHEMA details for table `{PROJECT_ID}.{dataset_id}.bc_product`]:
-Full table name: `{PROJECT_ID}.{dataset_id}.bc_product` - Column: product_id - Data Type: INTEGER - Primary Key: False - Foreign Key: True - Description: The unique identifier of the product. 
-Full table name: `{PROJECT_ID}.{dataset_id}.bc_product` - Column: product_name - Data Type: STRING - Primary Key: False - Foreign Key: False - Description: The name of the product. 
-Full table name: `{PROJECT_ID}.{dataset_id}.bc_product` - Column: product_type - Data Type: STRING - Primary Key: False - Foreign Key: False - Description: The type of the product (e.g., physical, digital). 
-Full table name: `{PROJECT_ID}.{dataset_id}.bc_product` - Column: price - Data Type: NUMERIC - Primary Key: False - Foreign Key: False - Description: The price of the product. 
+Full table name: `{PROJECT_ID}.{dataset_id}.bc_product` - Column: product_id - Data Type: INTEGER - Primary Key: False - Foreign Key: True - Description: The unique identifier of the product.
+Full table name: `{PROJECT_ID}.{dataset_id}.bc_product` - Column: product_name - Data Type: STRING - Primary Key: False - Foreign Key: False - Description: The name of the product.
+Full table name: `{PROJECT_ID}.{dataset_id}.bc_product` - Column: product_type - Data Type: STRING - Primary Key: False - Foreign Key: False - Description: The type of the product (e.g., physical, digital).
+Full table name: `{PROJECT_ID}.{dataset_id}.bc_product` - Column: price - Data Type: NUMERIC - Primary Key: False - Foreign Key: False - Description: The price of the product.
 Full table name: `{PROJECT_ID}.{dataset_id}.bc_product` - Column: last_updated_datetime - Data Type: DATETIME - Primary Key: False - Foreign Key: False - Description: The date and time when the product information was last updated.
 [SCHEMA details for table `{PROJECT_ID}.{dataset_id}.bc_order_line_items`]:
-Full table name: `{PROJECT_ID}.{dataset_id}.bc_order_line_items` - Column: order_line_item_id - Data Type: INTEGER - Primary Key: False - Foreign Key: False - Description: The unique identifier of the order line item. 
-Full table name: `{PROJECT_ID}.{dataset_id}.bc_order_line_items` - Column: order_id - Data Type: INTEGER - Primary Key: False - Foreign Key: True - Description: The identifier of the order associated with this line item.  
-Full table name: `{PROJECT_ID}.{dataset_id}.bc_order_line_items` - Column: product_id - Data Type: INTEGER - Primary Key: False - Foreign Key: False - Description: The identifier of the product associated with this line item.  
-Full table name: `{PROJECT_ID}.{dataset_id}.bc_order_line_items` - Column: quantity - Data Type: INTEGER - Primary Key: False - Foreign Key: False - Description: The quantity of the product in this line item.  
-Full table name: `{PROJECT_ID}.{dataset_id}.bc_order_line_items` - Column: total_inc_tax - Data Type: NUMERIC - Primary Key: False - Foreign Key: False - Description: The total amount of the line item including tax. 
-Full table name: `{PROJECT_ID}.{dataset_id}.bc_order_line_items` - Column: total_tax - Data Type: NUMERIC - Primary Key: False - Foreign Key: False - Description: The total tax amount for the line item. 
-Full table name: `{PROJECT_ID}.{dataset_id}.bc_order_line_items` - Column: is_refunded - Data Type: BOOLEAN - Primary Key: False - Foreign Key: False - Description: Indicates whether the line item has been refunded.  
+Full table name: `{PROJECT_ID}.{dataset_id}.bc_order_line_items` - Column: order_line_item_id - Data Type: INTEGER - Primary Key: False - Foreign Key: False - Description: The unique identifier of the order line item.
+Full table name: `{PROJECT_ID}.{dataset_id}.bc_order_line_items` - Column: order_id - Data Type: INTEGER - Primary Key: False - Foreign Key: True - Description: The identifier of the order associated with this line item.
+Full table name: `{PROJECT_ID}.{dataset_id}.bc_order_line_items` - Column: product_id - Data Type: INTEGER - Primary Key: False - Foreign Key: False - Description: The identifier of the product associated with this line item.
+Full table name: `{PROJECT_ID}.{dataset_id}.bc_order_line_items` - Column: quantity - Data Type: INTEGER - Primary Key: False - Foreign Key: False - Description: The quantity of the product in this line item.
+Full table name: `{PROJECT_ID}.{dataset_id}.bc_order_line_items` - Column: total_inc_tax - Data Type: NUMERIC - Primary Key: False - Foreign Key: False - Description: The total amount of the line item including tax.
+Full table name: `{PROJECT_ID}.{dataset_id}.bc_order_line_items` - Column: total_tax - Data Type: NUMERIC - Primary Key: False - Foreign Key: False - Description: The total tax amount for the line item.
+Full table name: `{PROJECT_ID}.{dataset_id}.bc_order_line_items` - Column: is_refunded - Data Type: BOOLEAN - Primary Key: False - Foreign Key: False - Description: Indicates whether the line item has been refunded.
 Full table name: `{PROJECT_ID}.{dataset_id}.bc_order_line_items` - Column: last_updated_datetime - Data Type: DATETIME - Primary Key: False - Foreign Key: False - Description: The date and time when the line item information was last updated.
 '''
 
+# Sample user question
 QUESTION = '[Q]: Give me last customer with date and name'
 
+# Sample examples for user questions and SQL queries
 examples = [
     {
-        "Question":"[Q]: tell me the customer who purchases most product 77?",
-        "SQL":f'''[SQL]: SELECT c.full_name
+        "Question": "[Q]: tell me the customer who purchases most product 77?",
+        "SQL": f'''[SQL]: SELECT c.full_name
             FROM `{PROJECT_ID}.{dataset_id}.bc_order_line_items` AS oli
             INNER JOIN `{PROJECT_ID}.{dataset_id}.bc_order` AS o ON oli.order_id = o.order_id
             INNER JOIN `{PROJECT_ID}.{dataset_id}.bc_product` AS p ON oli.product_id = p.product_id
@@ -96,7 +101,7 @@ examples = [
     }
 ]
 
-
+# Function to concatenate and display example questions and SQL queries
 def getExamples():
     r = ''
     for example in examples[:2]:
@@ -107,19 +112,20 @@ def getExamples():
     return r
 
 
+# Function to display a single user question
 def getQuestion(q):
     return "\r\n".join(['Here is an example of user question and answer SQL.', TABLE_SCHEMA_STR, q])
 
 
-parameters = {
-    "temperature": 0.2,
-    "max_output_tokens": 1024
-}
-
-
+# Function to handle generating SQL from user questions
 def handleQuestions(q):
+    # Parameters for text generation models
+    parameters = {
+        "temperature": 0.2,
+        "max_output_tokens": 1024
+    }
     prefix = '\r\n'.join([
-        'You are a SQL expert. Please convert text into GoogleSQL statement. We will first give the dataset schema and then ask a question in text. You are asked to generate SQL statement. Always use EXTRACT method instead of year and month',
+        'You are a SQL expert. Please convert text into GoogleSQL statement. We will first give the dataset schema and then ask a question in text. You are asked to generate SQL statement. Always use EXTRACT method instead of year and month.',
         getExamples(), getQuestion(q)])
     generation_model = CodeGenerationModel.from_pretrained("code-bison@001")
     response = generation_model.predict(prefix=prefix, **parameters)
@@ -132,7 +138,9 @@ def handleQuestions(q):
 handleQuestions(QUESTION)
 
 
+# Function to convert SQL response to natural language
 def handleSqlToNLP(gQueryStatement, userInput):
+    print(gQueryStatement)
     json_data = gQueryStatement.to_json(orient="records", indent=4)
     parameters = {
         "temperature": 0.2,
@@ -144,14 +152,15 @@ def handleSqlToNLP(gQueryStatement, userInput):
     response = model.predict(
         f"""This is the user query : {userInput}
         and here is json response from my database:
-        {json_data} Generate a description for the following in the natural language but don't use the word json in 
-        the statement. Pretend like you are interactive with the end user """,
+        {json_data}. Generate a description for the following in the natural language and if response from my database is empty then return no data found message
+         and don't use the word json in the statement. Pretend like you are interactive with the end user """,
         **parameters
     )
     print(response.text)
     return response.text
 
 
+# Function to handle exceptions when processing user questions
 def handleSqlToNLPException(e, userInput):
     pp = {
         "temperature": 0.2,
@@ -159,13 +168,12 @@ def handleSqlToNLPException(e, userInput):
         "top_p": 0.8,
         "top_k": 40
     }
-    print(e)
     model = TextGenerationModel.from_pretrained("text-bison@001")
     response = model.predict(
-        f"""This is the user query : {userInput} and I got the the exception from code because user ask something out 
-        of context and my AI model deals only with questions related merchant assistant i.e  Orders, products and 
+        f"""This is the user query : {userInput} and I got the the exception from code because user ask something out
+        of context and my AI model deals only with questions related merchant assistant i.e  Orders, products and
         customer related question. Exception: {e} .
-        Generate the apology message in the natural language and don't use any technical term in 
+        Generate the apology message in the natural language and don't use any technical term in
         the statement. Pretend like you are interactive with the end user, message should be precisely and do add some emojis in the message for good user experience.
         """,
         **pp
@@ -174,6 +182,7 @@ def handleSqlToNLPException(e, userInput):
     return response.text
 
 
+# Route for handling API requests
 @app.route('/api/bigquery', methods=['GET'])
 def hello_world():
     query = request.args.get('query')
@@ -194,5 +203,6 @@ def hello_world():
         return jsonify({"response": res})
 
 
+# Run the Flask app
 if __name__ == '__main__':
     app.run()
